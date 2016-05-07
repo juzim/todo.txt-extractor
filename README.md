8b622ee0f6f20abaf679ad44734f6f83b05739aa
Julian Zimmermann <julian.zimmermann@researchgate.net>
2016-05-08 00:18:04 +0200

    Added arguments

--- README.md
+++ README.md
@@ -3,18 +3,28 @@ Finds and extracts all todos of a project and saves them as a [todo.txt](http://
 Currently only php and js files are supported
 
 Usage:
-python3.5 extractTasks.py FOLDERPATH
+python3 extractTasks.py FOLDERPATH
+
+Aruguments:
+-o output file name
+-e (multiple) set file extensions, defaultsa to php, js and py
+-f parse single file
+
+Example:
+python3 extractTasks.py /home/thisisme/dev/kittensdeathmatch.com -e php -e js -o kittenstodo.txt
 
 Todo:
 - configurations for multiple projects
   - todo.txt file name
   - project path
   - file extensions
-- argument for parsing single files (so one can pipe search results into it)
+  - exclude folders
 - support for more languages (find all comment variations)
 - allow existing tasks in file
 - update existing tasks
 - remove deleted tasks/move to "done" file
+- don't overwrite existing files
+- add tutorials for using folder watchers to pass in changed files
 
 Things this won't do:
 - update task in source file when changed in txt
--- extractTasks.py
+++ extractTasks.py
@@ -1,31 +1,62 @@
 #@todo pass filename from outside for piping
 import glob
 import re
-from sys import argv
+import os
+import argparse
 
-script, basePath = argv
+parser = argparse.ArgumentParser("Extract tasks from files and creates a todo.txt")
 
-types = ('php', 'js')
+parser.add_argument('basePath', help='absolute path to project. Use "." for current directory')
+parser.add_argument('-o', action="store", dest="todoFile", help='absolute path for output file')
+parser.add_argument('-e', action="append", default=[], dest="types", help='File extensions')
+parser.add_argument('-f', action="store", dest="specificFile", help='Parson single file only')
+
+args = parser.parse_args()
+
+basePath = args.basePath
+
+if args.todoFile:
+    output = args.todoFile
+    if not output.endswith(".txt"):
+        output += '.txt'
+else:
+    output = os.getcwd() + "/todo.txt"
+
+print(output)
+
+types = args.types
+
+if len(types) == 0:
+    types = ['php', 'js', 'py']
+
+print(args.specificFile)
+
+if basePath == '.':
+    basePath = os.getcwd()
+
 files_grabbed = []
-for types in types:
-    files_grabbed.extend(glob.glob(basePath + '/**/*.' + types, recursive=True))
+
+if args.specificFile:
+    files_grabbed.append(args.specificFile)
+else:
+    for types in types:
+        files_grabbed.extend(glob.glob(basePath + '/**/*.' + types, recursive=True))
 
 foundTasks = []
 
 for path in files_grabbed:
     with open(path) as currentFile:
         for num, line in enumerate(currentFile, 1):
-            match = re.search("// ?@?(todo|TODO):? (.*)", line)
+            match = re.search("[/#] ?@?(todo|TODO):? (.*)", line)
             if match:
                 text = match.group(2).strip()
                 shortPath = re.sub(basePath + "/", '', path)
                 task = text + " @" + shortPath + ":" + str(num)
                 foundTasks.append(task)
 
-todoFile = "todo.txt"
-
-text_file = open(todoFile, "w+")
-
+text_file = open(output, "w+")
 text_file.write("\n".join(foundTasks))
-
 text_file.close()
+
+print(str(len(files_grabbed)) + " files parsed")
+print(str(len(foundTasks)) + " tasks added")
